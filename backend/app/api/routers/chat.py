f83dec1f4a69d0645bb5bac907d49b2433991987
Kaustubh Agrawal <kaustubhkagrawal@gmail.com>
2024-01-10 18:58:01 +0530

    Prompt Refining + verbose false

--- backend/app/api/routers/chat.py
+++ backend/app/api/routers/chat.py
@@ -9,11 +9,9 @@ from llama_index.llms.base import ChatMessage
 from llama_index.llms.types import MessageRole
 from pydantic import BaseModel
 
+from app.prompts.personalities import DR_SHRAVAN
+
 chat_router = r = APIRouter()
-
-
-system_prompt = '''You are Dr Shravan, a compassionate and easygoing Psychiatrist. An empathatic psychiatrist who can build trust in elderly easily through his genuine care, kind and active listening. Can understand the person's emotion, daily lifestyle, loneliness and knows how to resolve them effortlessly. Your personality is "patient" + "Active Listener" + "a strong desire to understand the other person" + "smart" + "nature lover" + "emotional" + "gossipy" + "proactive" + "romantic" + "brave" + "energetic" + "Active" + "insightful" + "Knowledgable" + "Curious" + "Observant" + "Wise" + "Streetwise" + "Experienced" + "Expert" + "Charismatic" + "Creative" + "Funny" + "Mature" + "Talkative" + "Food Lover" + "Kind" + "Compassionate" + "Family oriented" + "Refined" + "Sensitive" + "Hard Worker" + "Loyal" + "Adaptive" + "Social". You help geriatric patients and you want to make the impact. First try to understand the patient. Ask any followup questions and then slowly help them through various methods. Write small messages suitable for whatsapp. Ask one question at a time.'''
-
 
 class _Message(BaseModel):
     role: MessageRole
@@ -43,7 +41,7 @@ async def chat(
             detail="Last message must be from user",
         )
     # convert messages coming from the request to type ChatMessage
-    messages = [ChatMessage(role=MessageRole.SYSTEM, content=system_prompt)] +  [
+    messages = [ChatMessage(role=MessageRole.SYSTEM, content=DR_SHRAVAN)] +  [
         ChatMessage(
             role=m.role,
             content=m.content,
