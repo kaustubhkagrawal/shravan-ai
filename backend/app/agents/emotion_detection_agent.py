4b7df7831f063a451ed8b71220075795c47dce67
ajinkyaT <ajinkya.takawale97@gmail.com>
2024-01-11 10:59:28 +0530

    change to llm program

--- backend/app/agents/emotion_detection_agent.py
+++ backend/app/agents/emotion_detection_agent.py
@@ -8,7 +8,7 @@ from app.engine.context import create_service_context
 from llama_index.chat_engine.types import BaseChatEngine, ChatMode
 from llama_index.agent import ReActAgent
 
-from llama_index.program import GuidancePydanticProgram
+from llama_index.program import LLMTextCompletionProgram
 
 from app.api.routers.chat import _Message
 
@@ -20,19 +20,19 @@ class EmotionOutput(BaseModel):
 pplx_api_key = os.environ.get("PPLX_API_KEY", None)
 
 async def get_emotion_detection_output(chat_messages: str):
-    program = GuidancePydanticProgram(
+    program = LLMTextCompletionProgram(
         output_cls=EmotionOutput,
         prompt_template_str=(
             "Generate an output emotional label among [angry, calm, fearful, happy, sad], given the conversation history so far."
             " The conversation history is as follows:\n\n"
             "{{chat_messages}}\n"
         ),
-        guidance_llm=Perplexity(
-            api_key=pplx_api_key, model="mistral-8x7b-instruct", temperature=1
-        ),
+        # guidance_llm=Perplexity(
+        #     api_key=pplx_api_key, model="mistral-8x7b-instruct", temperature=1
+        # ),
         verbose=False,
     )
 
-    return program(conversation_history=chat_messages)
+    return program(chat_messages=chat_messages)
 
 
