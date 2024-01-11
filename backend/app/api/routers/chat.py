2430902f8a57ccfa98ad508772ddfbe4c2a2a628
ajinkyaT <ajinkya.takawale97@gmail.com>
2024-01-11 08:53:06 +0530

    refactor chat.py file

--- backend/app/api/routers/chat.py
+++ backend/app/api/routers/chat.py
@@ -12,9 +12,9 @@ from llama_index.llms.base import ChatMessage
 from llama_index.llms.types import MessageRole
 from pydantic import BaseModel
 
-from app.prompts.system import SYSTEM_PROMPT
 
-system_prompt = ""
+with open("app/prompts/system_prompt.txt", "r") as file:
+        system_prompt = file.read()
 
 chat_router = r = APIRouter()
 
@@ -50,9 +50,6 @@ async def chat(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Last message must be from user",
         )
-
-    with open("app/prompts/system_prompt.txt", "r") as file:
-        system_prompt = file.read()
 
     # convert messages coming from the request to type ChatMessage
     messages = [ChatMessage(role=MessageRole.SYSTEM, content=system_prompt)] + [
@@ -107,3 +104,48 @@ async def voice(
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
         )
+    
+
+@r.post("/emotions")
+async def emotions(
+    request: Request,
+    data: ApiRequest,
+    chat_engine: BaseChatEngine = Depends(get_chat_engine),
+):
+    # check preconditions and get last message
+    if len(data.messages) == 0:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="No messages provided",
+        )
+    lastMessage = data.messages.pop()
+    if lastMessage.role != MessageRole.USER:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Last message must be from user",
+        )
+
+    with open("app/prompts/system_prompt.txt", "r") as file:
+        system_prompt = file.read()
+
+    # convert messages coming from the request to type ChatMessage
+    messages = [ChatMessage(role=MessageRole.SYSTEM, content=system_prompt)] + [
+        ChatMessage(
+            role=m.role,
+            content=m.content,
+        )
+        for m in data.messages
+    ]
+
+    # query chat engine
+    response = chat_engine.stream_chat(lastMessage.content, messages)
+
+    # stream response
+    async def event_generator():
+        for token in response.response_gen:
+            # If client closes connection, stop sending events
+            if await request.is_disconnected():
+                break
+            yield token
+
+    return StreamingResponse(event_generator(), media_type="text/plain")
